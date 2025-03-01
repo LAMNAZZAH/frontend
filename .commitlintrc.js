48b61233cce945d18b438e846e494ef6775224b0
LAMNAZZAH <medlmz01@gmail.com>
2025-03-01 18:40:53 +0000

    chore(test): vitest testing

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    // You can customize rules here
     'body-max-line-length': [2, 'always', 100],
     'subject-case': [0], // Disable case restrictions
   },
